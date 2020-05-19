64fb5405910832d5e80b52ddcb3ba3638968b544
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-05-19 22:26:04 +0100

    School result context added

---
+++ SchoolPerformance/SchoolPerformance/Models/SchoolResult.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Models
+{
+    public class SchoolResult
+    {
+        [Display(Name="Unique Reference Number")]
+        public int URN { get; set; }
+
+        [Display(Name = "Academic Year")]
+        public int ACADEMICYEAR { get; set; }
+
+        [Display(Name = "Attainment 8 Score")]
+        public double ATT8SCR { get; set; }
+
+        [Display(Name = "Attainment 8 Score Disadvataged Pupils")]
+        public double ATT8SCR_FSM6CLA1A { get; set; }
+
+        [Display(Name = "Attainment 8 Score Non-Disadvataged Pupils")]
+        public double ATT8SCR_NFSM6CLA1A { get; set; }
+
+        [Display(Name = "Progress 8 Score")]
+        public double P8MEA { get; set; }
+
+        [Display(Name = "Progress 8 Score Disadvataged Pupils")]
+        public double P8MEA_FSM6CLA1A { get; set; }
+
+        [Display(Name = "Progress 8 Score Non-Disadvataged Pupils")]
+        public double P8MEA_NFSM6CLA1A { get; set; }
+
+        [Display(Name = "Percentage of pupils achieving grade 9-4 in English and Maths")]
+        public double PTL2BASICS_94 { get; set; }
+
+        [Display(Name = "Percentage of Disadvataged pupils achieving grade 9-4 in English and Maths")]
+        public double PTFSM6CLA1ABASICS_94 { get; set; }
+
+        [Display(Name = "Percentage of Non-Disadvataged pupils achieving grade 9-4 in English and Maths")]
+        public double PTNOTFSM6CLA1ABASICS_94 { get; set; }
+
+        [Display(Name = "Percentage of pupils achieving grade 9-5 in English and Maths")]
+        public double PTL2BASICS_95 { get; set; }
+
+        [Display(Name = "Percentage of Disadvataged pupils achieving grade 9-5 in English and Maths")]
+        public double PTFSM6CLA1ABASICS_95 { get; set; }
+
+        [Display(Name = "Percentage of Non-Disadvataged pupils achieving grade 9-5 in English and Maths")]
+        public double PTNOTFSM6CLA1ABASICS_95 { get; set; }
+
+
+
+    }
+}
