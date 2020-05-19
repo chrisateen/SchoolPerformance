97f9981293eae69d2f0cd87916fe2245f11c9365
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-05-19 21:02:55 +0100

    School Model added

---
+++ SchoolPerformance/SchoolPerformance/Models/School.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Models
+{
+    public class School
+    {
+        [Display(Name="Unique Reference Number")]
+        public int URN { get; set; }
+
+        [Display(Name = "DfE Number")]
+        public int LAESTAB { get; set; }
+
+        [Display(Name = "School Name")]
+        public string SCHNAME { get; set; }
+    }
+}
