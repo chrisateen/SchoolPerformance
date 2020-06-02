84a368386896c3452eab3e8942522d24c9e77bef
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-06-02 22:49:45 +0100

    Registered DbContext

--- SchoolPerformance/SchoolPerformance/Models/SchoolPerformanceContext.cs
+++ SchoolPerformance/SchoolPerformance/Models/SchoolPerformanceContext.cs
@@ -18,11 +18,35 @@ namespace SchoolPerformance.Models
         public virtual DbSet<SchoolDetails> SchoolDetails { get; set; }
         public virtual DbSet<SchoolResult> SchoolResult { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //Specify that there is a composite keys in SchoolResult and SchoolContextual
+            modelBuilder.Entity<SchoolResult>()
+                .HasKey(c => new { c.URN, c.ACADEMICYEAR});
+
+            modelBuilder.Entity<SchoolContextual>()
+                .HasKey(c => new { c.URN, c.ACADEMICYEAR });
+
+            //Specify the one to one relationship between school and school details
+            modelBuilder.Entity<School>()
+                .HasOne<SchoolDetails>(s => s.SchoolDetails)
+                .WithOne(s => s.School)
+                .HasForeignKey<SchoolDetails>( s => s.URN);
+
+            //Specify the one to many relationship between school and school contextual
+            modelBuilder.Entity<School>()
+                .HasMany<SchoolContextual>(s => s.SchoolContextuals)
+                .WithOne(s => s.School)
+                .HasForeignKey(s => s.URN);
+
+            //Specify the one to many relationship between school and school result
+            modelBuilder.Entity<School>()
+                .HasMany<SchoolResult>(s => s.SchoolResults)
+                .WithOne(s => s.School)
+                .HasForeignKey(s => s.URN);
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
         }
 
--- SchoolPerformance/SchoolPerformance/Startup.cs
+++ SchoolPerformance/SchoolPerformance/Startup.cs
@@ -1,21 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SchoolPerformance.Models;
 
 namespace SchoolPerformance
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration _configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<SchoolPerformanceContext>(options =>
+                    options.UseSqlServer(
+                                _configuration.GetConnectionString("SchoolPerformanceDb")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
