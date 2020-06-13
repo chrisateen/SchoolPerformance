fa5ec59ca2b1be375a34fd61a65bea93ce2510a5
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-06-14 00:40:45 +0100

    Changed test to use sqlite

--- SchoolPerformance/SchoolPerformaceTest/SchoolResultRepositoryTest.cs
+++ SchoolPerformance/SchoolPerformaceTest/SchoolResultRepositoryTest.cs
@@ -11,112 +11,50 @@ namespace SchoolPerformaceTest
     [TestClass]
     public class SchoolResultRepositoryTest
     {
-        //Properties to hold mock data
-        IQueryable<School> _school { get; set; }
-        IQueryable<SchoolDetails> _schoolDetails { get; set; }
-        IQueryable<SchoolContextual> _schoolContextual { get; set; }
-        IQueryable<SchoolResult> _schoolResult { get; set; }
+        private SchoolPerformanceContext _context;
 
-        //Properties to hold mock DbSet
-        Mock<DbSet<School>> _mockSchoolDbSet { get; set; }
-        Mock<DbSet<SchoolContextual>> _mockSchoolContextualDbSet { get; set; }
-        Mock<DbSet<SchoolDetails>> _mockSchoolDetailsDbSet { get; set; }
-        Mock<DbSet<SchoolResult>> _mockSchoolResultDbSet { get; set; }
+        private ISchoolResultRepository<School> _repository;
 
-        //Properties to hold mock DbContext
-        Mock<SchoolPerformanceContext> _mockSchoolPerformanceDbContext { get; set; }
-
-        ISchoolResultRepository<School> _service { get; set; }
+        private List<School> _schools;
+        private List<SchoolResult> _schoolResults;
 
         [TestInitialize]
         public void Setup()
         {
-            createData();
-            setupDbContext();
-            _service = new SchoolResultRepository<School>(_mockSchoolPerformanceDbContext.Object);
-        }
+            //Arange
+            var connection = new InMemorySqliteConnection();
+            _context = connection._context;
+            _repository = connection._repository;
 
-        [Ignore]
-        public void createData()
-        {
-            //Mock data
-            _school = new List<School>
+            _schools = new List<School>
             {
                 new School { URN = 1, LAESTAB = 1,SCHNAME = "Test 1" },
                 new School { URN = 2, LAESTAB = 2,SCHNAME = "Test 2" }
-            }.AsQueryable();
+            };
 
-            _mockSchoolDbSet = _school.setupDbSet();
-
-            _schoolDetails = new List<SchoolDetails>
-            {
-                new SchoolDetails { URN = 1, GENDER = "Mixed" },
-                new SchoolDetails { URN = 2, GENDER = "Mixed" }
-            }.AsQueryable();
-
-            _mockSchoolDetailsDbSet = _schoolDetails.setupDbSet();
-
-            _schoolContextual = new List<SchoolContextual>
-            {
-                new SchoolContextual { URN = 1, ACADEMICYEAR = 2018, NOR = 1200 },
-                new SchoolContextual { URN = 1, ACADEMICYEAR = 2019, NOR = 1100 },
-                new SchoolContextual { URN = 2, ACADEMICYEAR = 2018, NOR = 900 },
-                new SchoolContextual { URN = 2, ACADEMICYEAR = 2019, NOR = 1000 }
-            }.AsQueryable();
-
-            _mockSchoolContextualDbSet = _schoolContextual.setupDbSet();
-
-            _schoolResult = new List<SchoolResult>
+            _schoolResults = new List<SchoolResult>
             {
                 new SchoolResult { URN = 1, ACADEMICYEAR = 2018, PTL2BASICS_94 = 0.5 },
                 new SchoolResult{ URN = 1, ACADEMICYEAR = 2019, PTL2BASICS_94 = 0.51 },
                 new SchoolResult { URN = 2, ACADEMICYEAR = 2018, PTL2BASICS_94 = 0.62 },
                 new SchoolResult { URN = 2, ACADEMICYEAR = 2019, PTL2BASICS_94 = 0.68 }
-            }.AsQueryable();
+            };
 
-            _mockSchoolResultDbSet = _schoolResult.setupDbSet();
+            //Act    
+            _schools.ForEach(x => _context.School.Add(x));
+            _schoolResults.ForEach(x => _context.SchoolResult.Add(x));
+            _context.SaveChanges();
         }
 
-        [Ignore]
-        public void setupDbContext()
-        {
-            _mockSchoolPerformanceDbContext = new Mock<SchoolPerformanceContext>();
-            _mockSchoolPerformanceDbContext
-                .Setup(c => c.Set<School>())
-                .Returns(_mockSchoolDbSet.Object);
-            _mockSchoolPerformanceDbContext
-               .Setup(c => c.Set<SchoolContextual>())
-               .Returns(_mockSchoolContextualDbSet.Object);
-            _mockSchoolPerformanceDbContext
-              .Setup(c => c.Set<SchoolDetails>())
-              .Returns(_mockSchoolDetailsDbSet.Object);
-            _mockSchoolPerformanceDbContext
-              .Setup(c => c.Set<SchoolResult>())
-              .Returns(_mockSchoolResultDbSet.Object);
-        }
-
-        //Tests that the get() method in the repository class returns the 2 mock _school data
         [TestMethod]
-        public void RepositoryGetMethodReturnsSchools()
+        public void Task_Add_With_Relation()
         {
-            var schools = _service.Get();
-            var schoolsLstSize = schools.Count();
             
-            Assert.AreEqual(_school.Count(), schoolsLstSize);
-        }
+            //Assert   
+            var schoolLst = _repository.GetAll(x => x.OrderBy(n => n.SCHNAME), x => x.SchoolResults);
+            var resultCount = schoolLst.SelectMany(s => s.SchoolResults.Select(x => x.URN)).Count();
 
-        //Tests that the get() method with parameters in the repository class returns the 2 mock _school data
-        [TestMethod]
-        public void RepositoryGetMethodwithConditionsReturnsSchools()
-        {
-            //Conditions are ignored
-            var schools = _service.Get(x => x.URN == 1,
-                x => x.OrderBy(s => s.SCHNAME),
-                x => x.SchoolResults
-                );
-            var schoolsLstSize = schools.Count();
-
-            Assert.AreEqual(_school.Count(), schoolsLstSize);
+            Assert.AreEqual(_schoolResults.Count(), resultCount);
         }
 
 
---
+++ SchoolPerformance/SchoolPerformaceTest/SqliteConnection.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SchoolPerformance.Models;
+using SchoolPerformance.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPerformaceTest
+{
+    public class InMemorySqliteConnection : IDisposable
+    {
+        private readonly string _connectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        public readonly SchoolPerformanceContext _context;
+
+        public readonly ISchoolResultRepository<School> _repository;
+
+        public InMemorySqliteConnection()
+        {
+            _connection = new SqliteConnection(_connectionString);
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<SchoolPerformanceContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            _context = new SchoolPerformanceContext(options);
+
+            if (_context != null)
+            {
+                _context.Database.EnsureDeleted();
+                
+            }
+
+            _context.Database.EnsureCreated();
+
+            _repository = new SchoolResultRepository<School>(_context);
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
