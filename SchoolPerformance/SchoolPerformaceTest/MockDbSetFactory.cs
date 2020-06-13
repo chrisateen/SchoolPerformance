c587354c62c311abfccf002dc880b158a1082cc5
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-06-13 21:41:24 +0100

    Tests for repository added

---
+++ SchoolPerformance/SchoolPerformaceTest/MockDbSetFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Linq;
+
+namespace SchoolPerformaceTest
+{
+    public static class MockDbSetFactory
+    {
+        public static Mock<DbSet<T>> setupDbSet<T>(this IQueryable<T> source) where T : class
+        {
+            var mockDbSet = new Mock<DbSet<T>>();
+
+            mockDbSet.As<IQueryable<T>>()
+                .Setup(x => x.Provider)
+                .Returns(source.Provider);
+
+            mockDbSet.As<IQueryable<T>>()
+                .Setup(x => x.Expression)
+                .Returns(source.Expression);
+
+            mockDbSet.As<IQueryable<T>>()
+                .Setup(x => x.ElementType)
+                .Returns(source.ElementType);
+
+            mockDbSet.As<IQueryable<T>>()
+                .Setup(x => x.GetEnumerator())
+                .Returns(source.GetEnumerator());
+
+            return mockDbSet;
+        }
+
+    }
+}
--- SchoolPerformance/SchoolPerformaceTest/SchoolResultRepositoryTest.cs
+++ SchoolPerformance/SchoolPerformaceTest/SchoolResultRepositoryTest.cs
@@ -1,30 +1,124 @@
 using SchoolPerformance.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using SchoolPerformance.Repository;
 
 namespace SchoolPerformaceTest
 {
+    [TestClass]
     public class SchoolResultRepositoryTest
     {
+        //Properties to hold mock data
+        IQueryable<School> _school { get; set; }
+        IQueryable<SchoolDetails> _schoolDetails { get; set; }
+        IQueryable<SchoolContextual> _schoolContextual { get; set; }
+        IQueryable<SchoolResult> _schoolResult { get; set; }
 
-        IQueryable<School> _school = new List<School>
+        //Properties to hold mock DbSet
+        Mock<DbSet<School>> _mockSchoolDbSet { get; set; }
+        Mock<DbSet<SchoolContextual>> _mockSchoolContextualDbSet { get; set; }
+        Mock<DbSet<SchoolDetails>> _mockSchoolDetailsDbSet { get; set; }
+        Mock<DbSet<SchoolResult>> _mockSchoolResultDbSet { get; set; }
+
+        //Properties to hold mock DbContext
+        Mock<SchoolPerformanceContext> _mockSchoolPerformanceDbContext { get; set; }
+
+        ISchoolResultRepository<School> _service { get; set; }
+
+        [TestInitialize]
+        public void Setup()
         {
-            new School { URN = 1, LAESTAB = 1,SCHNAME = "Test 1" },
-            new School { URN = 2, LAESTAB = 2,SCHNAME = "Test 2" }
-        }.AsQueryable();
-
-        IQueryable<SchoolDetails> _schoolDetails = new List<SchoolDetails>
-        {
-            new SchoolDetails { URN = 1, GENDER = "Mixed" },
-            new SchoolDetails { URN = 2, GENDER = "Mixed" }
-        }.AsQueryable();
-
-        [Fact]
-        public void Test1()
-        {
-
+            createData();
+            setupDbContext();
+            _service = new SchoolResultRepository<School>(_mockSchoolPerformanceDbContext.Object);
         }
+
+        [Ignore]
+        public void createData()
+        {
+            //Mock data
+            _school = new List<School>
+            {
+                new School { URN = 1, LAESTAB = 1,SCHNAME = "Test 1" },
+                new School { URN = 2, LAESTAB = 2,SCHNAME = "Test 2" }
+            }.AsQueryable();
+
+            _mockSchoolDbSet = _school.setupDbSet();
+
+            _schoolDetails = new List<SchoolDetails>
+            {
+                new SchoolDetails { URN = 1, GENDER = "Mixed" },
+                new SchoolDetails { URN = 2, GENDER = "Mixed" }
+            }.AsQueryable();
+
+            _mockSchoolDetailsDbSet = _schoolDetails.setupDbSet();
+
+            _schoolContextual = new List<SchoolContextual>
+            {
+                new SchoolContextual { URN = 1, ACADEMICYEAR = 2018, NOR = 1200 },
+                new SchoolContextual { URN = 1, ACADEMICYEAR = 2019, NOR = 1100 },
+                new SchoolContextual { URN = 2, ACADEMICYEAR = 2018, NOR = 900 },
+                new SchoolContextual { URN = 2, ACADEMICYEAR = 2019, NOR = 1000 }
+            }.AsQueryable();
+
+            _mockSchoolContextualDbSet = _schoolContextual.setupDbSet();
+
+            _schoolResult = new List<SchoolResult>
+            {
+                new SchoolResult { URN = 1, ACADEMICYEAR = 2018, PTL2BASICS_94 = 0.5 },
+                new SchoolResult{ URN = 1, ACADEMICYEAR = 2019, PTL2BASICS_94 = 0.51 },
+                new SchoolResult { URN = 2, ACADEMICYEAR = 2018, PTL2BASICS_94 = 0.62 },
+                new SchoolResult { URN = 2, ACADEMICYEAR = 2019, PTL2BASICS_94 = 0.68 }
+            }.AsQueryable();
+
+            _mockSchoolResultDbSet = _schoolResult.setupDbSet();
+        }
+
+        [Ignore]
+        public void setupDbContext()
+        {
+            _mockSchoolPerformanceDbContext = new Mock<SchoolPerformanceContext>();
+            _mockSchoolPerformanceDbContext
+                .Setup(c => c.Set<School>())
+                .Returns(_mockSchoolDbSet.Object);
+            _mockSchoolPerformanceDbContext
+               .Setup(c => c.Set<SchoolContextual>())
+               .Returns(_mockSchoolContextualDbSet.Object);
+            _mockSchoolPerformanceDbContext
+              .Setup(c => c.Set<SchoolDetails>())
+              .Returns(_mockSchoolDetailsDbSet.Object);
+            _mockSchoolPerformanceDbContext
+              .Setup(c => c.Set<SchoolResult>())
+              .Returns(_mockSchoolResultDbSet.Object);
+        }
+
+        //Tests that the get() method in the repository class returns the 2 mock _school data
+        [TestMethod]
+        public void RepositoryGetMethodReturnsSchools()
+        {
+            var schools = _service.Get();
+            var schoolsLstSize = schools.Count();
+            
+            Assert.AreEqual(_school.Count(), schoolsLstSize);
+        }
+
+        //Tests that the get() method with parameters in the repository class returns the 2 mock _school data
+        [TestMethod]
+        public void RepositoryGetMethodwithConditionsReturnsSchools()
+        {
+            //Conditions are ignored
+            var schools = _service.Get(x => x.URN == 1,
+                x => x.OrderBy(s => s.SCHNAME),
+                x => x.SchoolResults
+                );
+            var schoolsLstSize = schools.Count();
+
+            Assert.AreEqual(_school.Count(), schoolsLstSize);
+        }
+
+
     }
 }
