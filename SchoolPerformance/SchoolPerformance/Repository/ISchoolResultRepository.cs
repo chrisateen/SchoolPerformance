961ec0b556364016bb83bd774768ba612ecd52f0
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-06-13 15:20:06 +0100

    Added generic repository

---
+++ SchoolPerformance/SchoolPerformance/Repository/ISchoolResultRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Repository
+{
+    /// <summary>
+    /// Interface for the repository class using generics
+    /// Enables one to get records from the database
+    /// </summary>
+    public interface ISchoolResultRepository<T> where T : class
+    {
+        /// <summary>
+        /// Gets all records from a DbSet.
+        /// </summary>
+        IEnumerable<T> GetAll(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                params Expression<Func<T, object>>[] includes);
+
+        IEnumerable<T> Get();
+        
+
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] includes);
+    }
+}
