d75e361b7298b81cc5f9411f2155f4c0e5165d65
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-06-24 18:13:10 +0100

    SchoolDetailsMap updated

--- SchoolPerformance/SchoolPerformance/LoadData/SchoolDetailsMap.cs
+++ SchoolPerformance/SchoolPerformance/LoadData/SchoolDetailsMap.cs
@@ -11,14 +11,15 @@ namespace LoadData
     {
         public SchoolDetailsMap()
         {
-            Map(m => m.URN);
-            Map(m => m.STREET);
-            Map(m => m.LOCALITY);
-            Map(m => m.ADDRESS3);
-            Map(m => m.TOWN);
-            Map(m => m.POSTCODE);
-
-
+            Map(m => m.URN).TypeConverter<NullURNAndLAESTABConverter>();
+            Map(m => m.STREET).TypeConverter<NullStringConverter>();
+            Map(m => m.LOCALITY).TypeConverter<NullStringConverter>();
+            Map(m => m.ADDRESS3).TypeConverter<NullStringConverter>();
+            Map(m => m.TOWN).TypeConverter<NullStringConverter>();
+            Map(m => m.POSTCODE).TypeConverter<NullStringConverter>();
+            Map(m => m.SCHOOLTYPE).TypeConverter<NullStringConverter>();
+            Map(m => m.GENDER).TypeConverter<NullStringConverter>();
+            Map(m => m.RELCHAR).TypeConverter<NullStringConverter>();
         }
     }
 }
