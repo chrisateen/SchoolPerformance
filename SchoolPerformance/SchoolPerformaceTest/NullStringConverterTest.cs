cc7ace6ded0eff46549f52067bab9165cfc67f01
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-06-25 20:27:40 +0100

    Null String and URN converter tests added

---
+++ SchoolPerformance/SchoolPerformaceTest/NullStringConverterTest.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration;
+using LoadData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPerformaceTest
+{
+    [TestClass]
+    public class NullStringConverterTest
+    {
+
+        //Tests NullStringConverter handles strings
+        [TestMethod]
+        public void NullStringConverterReturnsStrings()
+        {
+            //Arrange
+            var converter = new NullStringConverter();
+
+            //Act
+            var str = "Hello World !";
+
+            //Assert
+            Assert.AreEqual(str,converter.ConvertFromString(str, null, new MemberMapData(null)));
+        }
+
+        //Tests NullStringConverter is able to handle an empty string
+        [TestMethod]
+        public void NullStringConverterConvertsEmptyString()
+        {
+            //Arrange
+            var converter = new NullStringConverter();
+
+            //Act
+            var str = "";
+
+            //Assert
+            Assert.AreEqual(str, converter.ConvertFromString(str, null, new MemberMapData(null)));
+        }
+
+        
+    }
+}
---
+++ SchoolPerformance/SchoolPerformaceTest/NullURNAndLAESTABConverterTest.cs
@@ -0,0 +1,44 @@
+﻿using CsvHelper.Configuration;
+using LoadData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPerformaceTest
+{
+    [TestClass]
+    public class NullURNAndLAESTABConverterTest
+    {
+
+        //Tests NullURNAndLAESTABConverter is able to convert an int of type string to an integer
+        [TestMethod]
+        public void NullURNAndLAESTABConverterReturnsStrings()
+        {
+            //Arrange
+            var converter = new NullURNAndLAESTABConverter();
+
+            //Act
+            var num = "123456";
+
+            //Assert
+            Assert.AreEqual(Convert.ToInt32(num),converter.ConvertFromString(num, null, new MemberMapData(null)));
+        }
+
+        //Tests NullURNAndLAESTABConverterTest handles an empty string by returning zero
+        [TestMethod]
+        public void NullURNAndLAESTABConverterConvertsEmptyString()
+        {
+            //Arrange
+            var converter = new NullURNAndLAESTABConverter();
+
+            //Act
+            var str = "";
+
+            //Assert
+            Assert.AreEqual(0, converter.ConvertFromString(str, null, new MemberMapData(null)));
+        }
+
+        
+    }
+}
