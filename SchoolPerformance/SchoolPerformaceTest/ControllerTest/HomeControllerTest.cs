8256858ba1e4d8da04d744d9d219f9d9d43cb7aa
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-07-02 18:22:01 +0100

    Home Controller Test added

---
+++ SchoolPerformance/SchoolPerformaceTest/ControllerTest/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolPerformance.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPerformaceTest.ControllerTest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexReturnsHomePage()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
