735e0213927941a97e7b20813feee20df5a1cca3
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-07-03 21:19:06 +0100

    Scatterplot Controller added

---
+++ SchoolPerformance/SchoolPerformance/Controllers/ScatterplotController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolPerformance.Models;
+using SchoolPerformance.Repository;
+
+namespace SchoolPerformance.Controllers
+{
+    public class ScatterplotController : Controller
+    {
+
+        private ISchoolResultRepository<SchoolResult> _result;
+
+        public ScatterplotController(ISchoolResultRepository<SchoolResult> result)
+        {
+            _result = result;
+        }
+
+
+        public IActionResult Index()
+        {
+            var result = _result.GetAll(r => r.OrderBy(s => s.School.SCHNAME),r => r.School);
+
+            return View(result);
+        }
+    }
+}
