8b2c111f71c114ca4595c7449c305be30f4dc0f9
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-07-14 00:41:33 +0100

    Conversion from list of SchoolResult to list of ScatterplotViewModel added

---
+++ SchoolPerformance/SchoolPerformance/ViewModels/ImplicitConversion.cs
@@ -0,0 +1,33 @@
+﻿using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.ViewModels
+{
+    public static class ImplicitConversion
+    {
+        /// <summary>
+        /// Converts a list of SchoolResult to
+        /// a list of ScatterplotViewModel
+        /// </summary>
+        /// <param name="source">A list of SchoolResult</param>
+        /// <returns>A list of ScatterplotViewModel</returns>
+        public static List<ScatterplotViewModel> Convert (this List<SchoolResult> source)
+        {
+            List<ScatterplotViewModel> output = new List<ScatterplotViewModel>();
+
+            foreach(var item in source)
+            {
+                //implicit convertion from SchoolResult to ScatterplotViewModel
+                ScatterplotViewModel viewModel = item;
+                output.Add(viewModel);
+            }
+
+            return output;
+        }
+
+    }
+
+}
