261672bd48353bef07356a9abb359982d9b05642
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-07-16 17:12:25 +0100

    Table controller test added

---
+++ SchoolPerformance/SchoolPerformaceTest/ControllerTest/TableControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using FluentAssertions.AspNetCore.Mvc;
+using Moq;
+using SchoolPerformance.Controllers;
+using SchoolPerformance.Models;
+using SchoolPerformance.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SchoolPerformance.ViewModels;
+
+namespace SchoolPerformanceTest.ControllerTest
+{
+    [TestClass]
+    public class TableControllerTest
+    {
+        Mock<ISchoolResultRepository<SchoolResult>> _mockSchoolResult;
+        TableController _controller;
+
+        //Arrange
+        [TestInitialize]
+        public void Setup()
+        {
+            //Instantiate the controller class by mocking the repository
+            _mockSchoolResult = new Mock<ISchoolResultRepository<SchoolResult>>();
+            _controller = new TableController(_mockSchoolResult.Object);
+        }
+
+        //Checks Table view is rendered
+        //with an IEnumerable list of data of type TableViewModel
+        [TestMethod]
+        public void IndexReturnsHomePageWithScatterplotViewModel()
+        {
+
+            // Act and Assert
+            _controller.Index().Should()
+                .BeViewResult().WithDefaultViewName();
+
+            var res = _controller.Index().Should()
+                .BeOfType<ViewResult>().Subject;
+
+            var test = res.Model.Should()
+                .BeAssignableTo<IEnumerable<TableViewModel>>().Subject;
+        }
+    }
+}
