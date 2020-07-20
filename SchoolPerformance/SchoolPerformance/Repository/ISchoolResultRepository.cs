31b777568c4e31fd856a997b2354bf62e706b902
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-07-20 23:54:47 +0100

    Add SchoolResultRepository class and interface

---
+++ SchoolPerformance/SchoolPerformance/Repository/ISchoolResultRepository.cs
@@ -0,0 +1,21 @@
+﻿using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Repository
+{
+    /// <summary>
+    /// Enables one to remove records with no school results
+    /// </summary>
+    public interface ISchoolResultRepository
+    {
+        /// <summary>
+        /// Removes any records that have null
+        /// attainment or progress results
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SchoolResult> removeNullResults();
+    }
+}
---
+++ SchoolPerformance/SchoolPerformance/Repository/SchoolResultRepository.cs
@@ -0,0 +1,16 @@
+﻿using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Repository
+{
+    public class SchoolResultRepository:ISchoolResultRepository
+    {
+        public IEnumerable<SchoolResult> removeNullResults()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
