24df9a5e30586d725fe723b889e46ff17273c721
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-07-21 00:43:32 +0100

    SchoolResultRepository gets all properties in SchoolResult

--- SchoolPerformance/SchoolPerformance/Repository/SchoolResultRepository.cs
+++ SchoolPerformance/SchoolPerformance/Repository/SchoolResultRepository.cs
@@ -10,7 +10,18 @@ namespace SchoolPerformance.Repository
     {
         public IEnumerable<SchoolResult> removeNullResults()
         {
+            //Get all properties in school result
+            var allPropLst = typeof(SchoolResult).GetProperties();
+
+           //Remove properties School, URN and Academic Year
+            var propLst = allPropLst.Where(x => x.Name != "URN" || 
+            x.Name != "ACADEMICYEAR" || x.Name != "School");
+            
             throw new NotImplementedException();
         }
+
+
+        //Count all null result method - define in interface and make private
+        //get properties method - define in interface and make private
     }
 }
