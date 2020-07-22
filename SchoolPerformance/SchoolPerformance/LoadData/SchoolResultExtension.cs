ef4b23b0dfde24decad292cde0da535d677e6fb5
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-07-22 01:30:06 +0100

    Removed use of reflection from SchoolResultExtensions

--- SchoolPerformance/SchoolPerformance/LoadData/SchoolResultExtension.cs
+++ SchoolPerformance/SchoolPerformance/LoadData/SchoolResultExtension.cs
@@ -1,4 +1,5 @@
-﻿using SchoolPerformance.Models;
+﻿using FastMember;
+using SchoolPerformance.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,8 @@ namespace SchoolPerformance.LoadData
     /// </summary>
     public static class SchoolResultExtension
     {
-        private static IEnumerable<PropertyInfo> _properties;
+        //private static IEnumerable<string> _properties;
+        //private static TypeAccessor _accessor;
 
         /// <summary>
         /// Remove any SchoolResult object where all the attainment and progress properties are null
@@ -21,55 +23,76 @@ namespace SchoolPerformance.LoadData
         /// <returns>Returns a IEnumerable list of SchoolResult with null results removed</returns>
         public static IEnumerable<SchoolResult> removeNullResults(this IEnumerable<SchoolResult> results)
         {
-            //Get all the properties
-            _properties = getProperties();
 
-            //Remove schools that do not have any results
-            var filteredResults = results.Where(x => resultsNotEmpty(x));
+            return results.Where(
+                r => r.ATT8SCR != null ||
+                r.ATT8SCR_FSM6CLA1A != null ||
+                r.ATT8SCR_NFSM6CLA1A != null ||
+                r.P8MEA != null ||
+                r.P8MEA_FSM6CLA1A != null ||
+                r.P8MEA_NFSM6CLA1A != null ||
+                r.PTFSM6CLA1ABASICS_94 != null ||
+                r.PTFSM6CLA1ABASICS_95 != null ||
+                r.PTL2BASICS_94 != null ||
+                r.PTL2BASICS_95 != null ||
+                r.PTNOTFSM6CLA1ABASICS_94 != null ||
+                r.PTNOTFSM6CLA1ABASICS_95 != null
+                );
 
-            return filteredResults;
+            //_properties = getProperties();
+
+            //var schoolResultObj = new SchoolResult();
+
+            //_accessor = TypeAccessor.Create(schoolResultObj.GetType());
+
+            //return results.Where(r => resultsNotEmpty(r));
         }
 
-        /// <summary>
-        /// Get all properties of SchoolResult excluding URN and ACADEMICYEAR and School
-        /// </summary>
-        /// <returns>
-        /// IEnumerable list of properties in SchoolResult
-        /// </returns>
-        private static IEnumerable<PropertyInfo> getProperties()
-        {
-            //Get all properties in school result
-            var allProperties = typeof(SchoolResult).GetProperties();
+        ///// <summary>
+        ///// Get all properties of SchoolResult excluding URN and ACADEMICYEAR and School
+        ///// </summary>
+        ///// <returns>
+        ///// IEnumerable list of properties in SchoolResult
+        ///// </returns>
+        //private static IEnumerable<String> getProperties()
+        //{
+        //    //Get all properties in school result
+        //    var allProperties = typeof(SchoolResult).GetProperties();
 
-            //Remove properties School, URN and Academic Year
-            var propLst = allProperties.Where(x => x.Name != "URN"
-            && x.Name != "ACADEMICYEAR" && x.Name != "School");
+        //    //Remove properties School, URN and Academic Year
+        //    var propLst = allProperties.Where(x => x.Name != "URN"
+        //    && x.Name != "ACADEMICYEAR" && x.Name != "School");
 
-            return propLst;
-        }
+        //    var propStr = propLst.Select(p => p.Name);
 
-        /// <summary>
-        /// Checks if all properties are null
-        /// </summary>
-        /// <param name="result">SchoolResult object to check</param>
-        /// <returns>Returns if some properties are not null</returns>
-        private static bool resultsNotEmpty (SchoolResult result)
-        {
-            var includeSchool = false;
+        //    return propStr;
+        //}
 
-            foreach (var prop in _properties)
-            {
-                //If one of the result property is not null then that school should be included
-                if(prop.GetValue(result) != null)
-                {
-                    includeSchool = true;
-                    return includeSchool;
-                }
-            };
+        ///// <summary>
+        ///// Checks if all properties are null
+        ///// </summary>
+        ///// <param name="result">SchoolResult object to check</param>
+        ///// <returns>Returns if some properties are not null</returns>
+        //private static bool resultsNotEmpty(SchoolResult result)
+        //{
+        //    var includeSchool = false;
 
-            return includeSchool;
+        //    foreach (var prop in _properties)
+        //    {
+        //        var data = _accessor[result, prop];
 
-        }
+        //        //If one of the result property is not null then that school should be included
+        //        if (data != null)
+        //        {
+        //            includeSchool = true;
+        //            return includeSchool;
+        //        }
+        //    };
+
+        //    return includeSchool;
+
+        //}
+
 
     }
 }
