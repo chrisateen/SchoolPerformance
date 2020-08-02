a55f13fbee2321e3fb9ce754708b69b4213b1e92
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-08-02 01:22:00 +0100

    Namespace error corrected in HomeController test

--- SchoolPerformance/SchoolPerformaceTest/ControllerTest/HomeControllerTest.cs
+++ SchoolPerformance/SchoolPerformaceTest/ControllerTest/HomeControllerTest.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SchoolPerformaceTest.ControllerTest
+namespace SchoolPerformanceTest.ControllerTest
 {
     [TestClass]
     public class HomeControllerTest
