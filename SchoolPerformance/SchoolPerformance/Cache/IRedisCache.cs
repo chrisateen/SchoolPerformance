f5a90070508c73b516f6a0b3e589ac7ea2b40ef2
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-08-22 22:07:12 +0100

    RedisCache interface added to help with mocking

---
+++ SchoolPerformance/SchoolPerformance/Cache/IRedisCache.cs
@@ -0,0 +1,12 @@
+﻿using SchoolPerformance.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.Cache
+{
+    public interface IRedisCache
+    {
+        Task<IEnumerable<ScatterplotViewModel>> getScatterplotData();
+        Task saveScatterplotData(IEnumerable<ScatterplotViewModel> scatterplotDataLst);
+    }
+}
