cd1447a20d25f0caf766c02d366ba976b4c10ca7
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-08-30 22:06:03 +0100

    Tests added for getting data for Table Controller from cache

--- SchoolPerformance/SchoolPerformaceTest/CacheTest/RedisCacheTest.cs
+++ SchoolPerformance/SchoolPerformaceTest/CacheTest/RedisCacheTest.cs
@@ -25,45 +25,31 @@ namespace SchoolPerformaceTest.CacheTest
             _redisDatabase = new Mock<IRedisDatabase>();
         }
 
-        //Tests that if items are not in the redis database (i.e keys could not be found)
+        //Tests that if the ScatterplotViewModel data 
+        //are not in the redis database (i.e keys could not be found)
         //an empty list is returned
         [TestMethod]
-        public async Task getScatterplotDataReturnsEmptyListIfItemsAreNotInCache()
+        public async Task GetScatterplotDataReturnsEmptyListIfItemsAreNotInCache()
         {
             //Arrange
-            //Create and return an empty list of keys
-            //when SearchKeysAsync is called
-            var emptyLstOfKeys = Task.FromResult<IEnumerable<String>>(null);
-            _redisDatabase.Setup(m => m.SearchKeysAsync(It.IsAny<string>())).Returns(emptyLstOfKeys);
-
-            _redisCacheClient = new Mock<IRedisCacheClient>();
-            _redisCacheClient.Setup(m => m.Db0).Returns(_redisDatabase.Object);
-
-            _redisCache = new RedisCache(_redisCacheClient.Object);
+            SetupEmptyMockCache();
 
             //Act
-            IEnumerable<ScatterplotViewModel> result = await _redisCache.getScatterplotData();
+            IEnumerable<ScatterplotViewModel> result = await _redisCache.GetScatterplotData();
 
             //Assert
             Assert.AreEqual(0,result.Count());
 
         }
 
-        //Tests that if items are not in the redis database (i.e keys could not be found)
-        //an empty list is returned
+        //Tests that if the ScatterplotViewModel data 
+        //is in the redis database (i.e keys could not be found)
+        //the list of ScatterplotViewModel data is returned
         [TestMethod]
-        public async Task getScatterplotDataReturnsDataInCache()
+        public async Task GetScatterplotDataReturnsDataInCache()
         {
             //Arrange
-            //Create and return a mock list of keys
-            //when SearchKeysAsync is called
-            var LstOfKeys = new List<String>
-            {
-                "ScatterplotViewModel1","ScatterplotViewModel2","ScatterplotViewModel3"
-            };
-
-            var taskLstOfKeys = Task.FromResult<IEnumerable<String>>(LstOfKeys);
-            _redisDatabase.Setup(m => m.SearchKeysAsync(It.IsAny<string>())).Returns(taskLstOfKeys);
+            SetupListOfKeys("ScatterplotViewModel", 3);
 
 
             //Create and return a list of scatterplotViewModel
@@ -75,21 +61,184 @@ namespace SchoolPerformaceTest.CacheTest
                 {"ScatterplotViewModel1", new ScatterplotViewModel{URN = 1 , SCHNAME = "Test 1"} }
             };
 
+            //Gets first school name when list is sorted in alphabetical order
+            var firstSchoolinLst = lstScatterplotViewModel.OrderBy(s => s.Value.SCHNAME).First().Value.SCHNAME;
+
             var taskLstScatterplotViewModel = Task.FromResult<IDictionary<String,ScatterplotViewModel>>(lstScatterplotViewModel);
             _redisDatabase.Setup(m => m.GetAllAsync<ScatterplotViewModel>(It.IsAny<List<string>>()))
                            .Returns(taskLstScatterplotViewModel);
 
+            SetupRedisCacheClient();
+
+            //Act
+            IEnumerable<ScatterplotViewModel> result = await _redisCache.GetScatterplotData();
+
+            //Assert
+            //Number of items retrieved from cache is correct
+            Assert.AreEqual(lstScatterplotViewModel.Count(), result.Count());
+
+            //List returned from cache is sorted in order
+            Assert.AreEqual(firstSchoolinLst, result.First().SCHNAME);
+        }
+
+
+        //Tests that if the TableViewModelAll data 
+        //are not in the redis database (i.e keys could not be found)
+        //an empty list is returned
+        [TestMethod]
+        public async Task GetTableDataAllReturnsEmptyListIfItemsAreNotInCache()
+        {
+            //Arrange
+            SetupEmptyMockCache();
+
+            //Act
+            IEnumerable<TableViewModelAll> result = await _redisCache.GetTableDataAll();
+
+            //Assert
+            Assert.AreEqual(0, result.Count());
+
+        }
+
+        //Tests that if the TableViewModelAll data 
+        //is in the redis database
+        //the list of TableViewModelAll data is returned
+        [TestMethod]
+        public async Task GetTableDataAllReturnsDataInCache()
+        {
+            //Arrange
+            SetupListOfKeys("TableViewModelAll", 3);
+
+
+            //Create and return a list of TableViewModelAll
+            //when GetAllAsync is called
+            var lstTableViewModel = new Dictionary<String, TableViewModelAll>
+            {
+                {"TableViewModelAll3", new TableViewModelAll{URN = 3 , SCHNAME = "Test 3"} },
+                {"TableViewModelAll2", new TableViewModelAll{URN = 2, SCHNAME = "Test 2"} },
+                {"TableViewModelAll1", new TableViewModelAll{URN = 1 , SCHNAME = "Test 1"} }
+            };
+
+            var taskLstTableViewModel = Task.FromResult<IDictionary<String, TableViewModelAll>>(lstTableViewModel);
+            _redisDatabase.Setup(m => m.GetAllAsync<TableViewModelAll>(It.IsAny<List<string>>()))
+                           .Returns(taskLstTableViewModel);
+
+            SetupRedisCacheClient();
+
+            //Gets first school name when list is sorted in alphabetical order
+            var firstSchoolinLst = lstTableViewModel.OrderBy(s => s.Value.SCHNAME).First().Value.SCHNAME;
+
+            //Act
+            IEnumerable<TableViewModelAll> result = await _redisCache.GetTableDataAll();
+
+            //Assert
+            //Number of items retrieved from cache is correct
+            Assert.AreEqual(lstTableViewModel.Count(), result.Count());
+
+            //List returned from cache is sorted in order
+            Assert.AreEqual(firstSchoolinLst, result.First().SCHNAME);
+
+        }
+
+        //Tests that if the TableViewModelDisadvantaged data 
+        //are not in the redis database (i.e keys could not be found)
+        //an empty list is returned
+        [TestMethod]
+        public async Task GetTableDataDisadvantagedReturnsEmptyListIfItemsAreNotInCache()
+        {
+            //Arrange
+            SetupEmptyMockCache();
+
+            //Act
+            IEnumerable<TableViewModelDisadvantaged> result = await _redisCache.GetTableDataDisadvantaged();
+
+            //Assert
+            Assert.AreEqual(0, result.Count());
+
+        }
+
+
+        //Tests that if the TableViewModelDisadvantaged data 
+        //is in the redis database
+        //the list of TableViewModelDisadvantaged data is returned
+        [TestMethod]
+        public async Task GetTableDataDisadvantagedReturnsDataInCache()
+        {
+            //Arrange
+            SetupListOfKeys("TableViewModelDisadvantaged", 3);
+
+
+            //Create and return a list of TableViewModelAll
+            //when GetAllAsync is called
+            var lstTableViewModel = new Dictionary<String, TableViewModelDisadvantaged>
+            {
+                {"TableViewModelDisadvantaged3", new TableViewModelDisadvantaged{URN = 3 , SCHNAME = "Test 3"} },
+                {"TableViewModelDisadvantaged2", new TableViewModelDisadvantaged{URN = 2, SCHNAME = "Test 2"} },
+                {"TableViewModelDisadvantaged1", new TableViewModelDisadvantaged{URN = 1 , SCHNAME = "Test 1"} }
+            };
+
+            var taskLstTableViewModel = Task.FromResult<IDictionary<String, TableViewModelDisadvantaged>>(lstTableViewModel);
+            _redisDatabase.Setup(m => m.GetAllAsync<TableViewModelDisadvantaged>(It.IsAny<List<string>>()))
+                           .Returns(taskLstTableViewModel);
+
+            SetupRedisCacheClient();
+
+            //Gets first school name when list is sorted in alphabetical order
+            var firstSchoolinLst = lstTableViewModel.OrderBy(s => s.Value.SCHNAME).First().Value.SCHNAME;
+
+            //Act
+            IEnumerable<TableViewModelDisadvantaged> result = await _redisCache.GetTableDataDisadvantaged();
+
+            //Assert
+            //Number of items retrieved from cache is correct
+            Assert.AreEqual(lstTableViewModel.Count(), result.Count());
+
+            //List returned from cache is sorted in order
+            Assert.AreEqual(firstSchoolinLst, result.First().SCHNAME);
+
+        }
+
+
+        //Create and return an empty list of keys
+        //when SearchKeysAsync is called
+        //in the get methods
+        [Ignore]
+        public void SetupEmptyMockCache()
+        {
+            //Arrange
+           
+            var emptyLstOfKeys = Task.FromResult<IEnumerable<String>>(null);
+            _redisDatabase.Setup(m => m.SearchKeysAsync(It.IsAny<string>())).Returns(emptyLstOfKeys);
+            SetupRedisCacheClient();
+
+        }
+
+        //Setup Mock redisCacheClient
+        [Ignore]
+        public void SetupRedisCacheClient()
+        {
+            //Arrange
             _redisCacheClient = new Mock<IRedisCacheClient>();
             _redisCacheClient.Setup(m => m.Db0).Returns(_redisDatabase.Object);
 
             _redisCache = new RedisCache(_redisCacheClient.Object);
-
-            //Act
-            IEnumerable<ScatterplotViewModel> result = await _redisCache.getScatterplotData();
-
-            //Assert
-            Assert.AreEqual(lstScatterplotViewModel.Count(), result.Count());
-
         }
+
+        //Create and return a mock list of keys
+        //when SearchKeysAsync is called
+        [Ignore]
+        public void SetupListOfKeys(string keyStartsWith, int noOfKeys)
+        {
+            var LstOfKeys = new List<String>();
+
+            for (int i = 1; i == noOfKeys; i++)
+            {
+                LstOfKeys.Add(keyStartsWith + i);
+            }
+
+            var taskLstOfKeys = Task.FromResult<IEnumerable<String>>(LstOfKeys);
+
+            _redisDatabase.Setup(m => m.SearchKeysAsync(It.IsAny<string>())).Returns(taskLstOfKeys);
+        }
+
     }
 }
