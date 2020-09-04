12cc8c71a275e6b2e206528d7f73bb9940bd002b
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-05 00:16:42 +0100

    School view model added

---
+++ SchoolPerformance/SchoolPerformance/ViewModels/SchoolViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.ViewModels
+{
+    public class SchoolViewModel
+    {
+        public SchoolResult ResultSchool;
+        public SchoolResult ResultNational;
+        public SchoolContextual ContextualSchool;
+        public SchoolContextual ContextualNational;
+    }
+}
