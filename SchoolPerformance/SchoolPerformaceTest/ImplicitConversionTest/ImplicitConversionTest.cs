6be36ca5501f69369884b7c127c09ba82564adcd
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-05 23:48:42 +0100

    ResultViewModel test added

--- SchoolPerformance/SchoolPerformaceTest/ImplicitConversionTest/ImplicitConversionTest.cs
+++ SchoolPerformance/SchoolPerformaceTest/ImplicitConversionTest/ImplicitConversionTest.cs
@@ -55,6 +55,51 @@ namespace SchoolPerformaceTest.ImplicitConversionTest
             Assert.AreEqual(result.School.SCHNAME, resultViewModel.SCHNAME);
         }
 
+        //Test conversion of an object
+        //from SchoolResult to ResultViewModel
+        [TestMethod]
+        public void ResultModelToResultViewModel()
+        {
+            //Arrange
+            SchoolResult result = new SchoolResult
+            {
+                URN = 1,
+                ACADEMICYEAR = 2019,
+                PTFSM6CLA1A = 0.5,
+                ATT8SCR = 40,
+                ATT8SCR_FSM6CLA1A = 38,
+                ATT8SCR_NFSM6CLA1A = 42,
+                P8MEA = 0.00,
+                P8MEA_FSM6CLA1A = -0.01,
+                P8MEA_NFSM6CLA1A = 0.00,
+                PTL2BASICS_94 = 0.55,
+                PTFSM6CLA1ABASICS_94 = 0.54,
+                PTNOTFSM6CLA1ABASICS_94 = 0.56,
+                PTL2BASICS_95 = 0.25,
+                PTFSM6CLA1ABASICS_95 = 0.22,
+                PTNOTFSM6CLA1ABASICS_95 = 0.28,
+                School = new School
+                {
+                    URN = 1,
+                    LA = 100,
+                    ESTAB = 1000,
+                    SCHNAME = "Test School"
+                }
+            };
+
+            //Act
+            ResultViewModel resultViewModel = result;
+
+            //Assert
+
+            //Checks the SchoolResult object gets converted to ResultViewModel
+            Assert.IsNotNull(resultViewModel);
+
+            //Checks the School name in the school model is included
+            //when converting from SchoolResult to ResultViewModel
+            Assert.AreEqual(result.School.SCHNAME, resultViewModel.SCHNAME);
+        }
+
         //Test conversion of a list of objects
         //from a list of SchoolResult to a list of ScatterplotViewModel
         [TestMethod]
