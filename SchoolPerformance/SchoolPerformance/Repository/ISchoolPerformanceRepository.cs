00c855e444c06e3f7f411bca3035cb9f62550780
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-12 21:35:02 +0100

    GetByUrnOrLAESATB method added to repository interface

--- SchoolPerformance/SchoolPerformance/Repository/ISchoolPerformanceRepository.cs
+++ SchoolPerformance/SchoolPerformance/Repository/ISchoolPerformanceRepository.cs
@@ -141,6 +141,70 @@ namespace SchoolPerformance.Repository
         public Task<IEnumerable<T>> GetNational(
             params Expression<Func<T, object>>[] includes);
 
+        /// <summary>
+        /// Get a school record by URN or LAESATB
+        /// </summary>
+        /// <param name="id">School URN or LAESATB to get a record for</param>
+        /// <param name="filter">
+        /// Use a function to specify how to filter the records. 
+        /// If filtering is not need enter null</param>
+        /// <param name="orderBy">
+        /// Use a function to specify how to order the records. 
+        /// If ordering is not need enter null</param>
+        /// <param name="includes">
+        /// Specify a list of other DbSets to be included
+        /// </param>
+        /// <returns>Returns an IEnumerable list</returns>
+        public Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] includes);
+
+
+        /// <summary>
+        /// Get a school record by URN or LAESATB
+        /// </summary>
+        /// <param name="id">School URN or LAESATB to get a record for</param>
+        /// <param name="filter">
+        /// Use a function to specify how to filter the records. 
+        /// If filtering is not need enter null</param>
+        /// <param name="includes">
+        /// Specify a list of other DbSets to be included
+        /// </param>
+        /// <returns>Returns an IEnumerable list</returns>
+        public Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id,
+            Expression<Func<T, bool>> filter = null,
+            params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Get a school record by URN or LAESATB
+        /// </summary>
+        /// <param name="id">School URN or LAESATB to get a record for</param>
+        /// <param name="orderBy">
+        /// Use a function to specify how to order the records. 
+        /// If ordering is not need enter null</param>
+        /// <param name="includes">
+        /// Specify a list of other DbSets to be included
+        /// </param>
+        /// <returns>Returns an IEnumerable list</returns>
+        public Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Get a school record by URN or LAESATB
+        /// </summary>
+        /// <param name="id">School URN or LAESATB to get a record for</param>
+        /// <param name="includes">
+        /// Specify a list of other DbSets to be included
+        /// </param>
+        /// <returns>Returns an IEnumerable list</returns>
+        public Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id,
+            params Expression<Func<T, object>>[] includes);
 
     }
 }
