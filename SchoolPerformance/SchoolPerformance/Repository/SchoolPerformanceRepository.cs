0a1ec5dcc1824c5a7473d641c65e110aadb56f0d
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-12 22:27:36 +0100

    GetByUrnOrLAESATB implemented

--- SchoolPerformance/SchoolPerformance/Repository/SchoolPerformanceRepository.cs
+++ SchoolPerformance/SchoolPerformance/Repository/SchoolPerformanceRepository.cs
@@ -237,6 +237,7 @@ namespace SchoolPerformance.Repository
         /// <param name="getNational">
         /// If one requires the national data or not
         /// </param>
+        /// <returns>Returns query with national condition</returns>
         private IQueryable<T> National(IQueryable<T> data,Boolean getNational)
         {
             
@@ -265,5 +266,203 @@ namespace SchoolPerformance.Repository
             return data.Provider.CreateQuery<T>(callExpression);
 
         }
+
+        public async Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id, 
+            Expression<Func<T, bool>> filter = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            //Distinguish between LAESTAB and URN
+            //as LAESTAB is 7 digits 
+            if (id.ToString().Length == 7)
+            {
+                query = LAESTABxpression(query, id);
+            }
+            else
+            {
+                query = URNExpression(query, id);
+            }
+            //Include/merge other DbSets into our query
+            query = AddDbSets(query, includes);
+
+            //Include filter and order by condition in the query
+            query = AddFilterQuery(query, filter);
+
+            query = AddOrderQuery(query, orderBy);
+
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id, 
+            Expression<Func<T, bool>> filter = null, 
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            //Distinguish between LAESTAB and URN
+            //as LAESTAB is 7 digits 
+            if (id.ToString().Length == 7)
+            {
+                query = LAESTABxpression(query, id);
+            }
+            else
+            {
+                query = URNExpression(query, id);
+            }
+            //Include/merge other DbSets into our query
+            query = AddDbSets(query, includes);
+
+            //Include filter condition in the query
+            query = AddFilterQuery(query, filter);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            //Distinguish between LAESTAB and URN
+            //as LAESTAB is 7 digits 
+            if (id.ToString().Length == 7)
+            {
+                query = LAESTABxpression(query, id);
+            }
+            else
+            {
+                query = URNExpression(query, id);
+            }
+            //Include/merge other DbSets into our query
+            query = AddDbSets(query, includes);
+
+            //Include order by condition in the query
+            query = AddOrderQuery(query, orderBy);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetByUrnOrLAESATB(
+            int id, 
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            //Distinguish between LAESTAB and URN
+            //as LAESTAB is 7 digits 
+            if (id.ToString().Length == 7)
+            {
+                query = LAESTABxpression(query, id);
+            }
+            else
+            {
+                query = URNExpression(query, id);
+            }
+            //Include/merge other DbSets into our query
+            query = AddDbSets(query, includes);
+
+            return await query.ToListAsync();
+        }
+
+
+        /// <summary>
+        /// Builds an expression to get data by URN
+        /// </summary>
+        /// <param name="data">
+        /// Query to include the condition
+        /// </param>
+        /// <param name="id">School URN</param>
+        /// /// <returns>Returns query with URN == id condition</returns>
+        private IQueryable<T> URNExpression(IQueryable<T> data, int id)
+        {
+            //Build the expression
+            ParameterExpression pe = Expression.Parameter(typeof(T), "s");
+
+            MemberExpression me = Expression.Property(pe, "URN");
+
+            ConstantExpression constant = Expression.Constant(id, typeof(int));
+
+            //Create the condition
+            Expression body = Expression.Equal(me, constant);
+
+
+            //Create the expression data.where(s => s.URN == id ) 
+            MethodCallExpression callExpression = Expression.Call(
+                    typeof(Queryable),
+                    "Where",
+                    new Type[] { data.ElementType },
+                    data.Expression,
+                    Expression.Lambda<Func<T, bool>>(body, new ParameterExpression[] { pe }));
+
+            // Return an executable query from the expression tree.
+            return data.Provider.CreateQuery<T>(callExpression);
+        }
+
+        private IQueryable<T> LAESTABxpression(IQueryable<T> data, int id)
+        {
+            //Get the first 3 digits of the 
+            //Id which is the LA number
+            var LA = id / 10000;
+
+            //Get the last 4 digits of the 
+            //Id which is the ESTAB number
+            var ESTAB = id - (LA * 10000);
+
+            ParameterExpression pe = Expression.Parameter(typeof(T), "s");
+
+            MemberExpression meLEA;
+            MemberExpression meESTAB;
+
+            
+            if (typeof(T) == typeof(School))
+            {
+                //Build a basic expression property where type is School
+
+                meLEA = Expression.Property(pe, "LA");
+
+                meESTAB = Expression.Property(pe, "ESTAB");
+            }
+
+            else
+            {
+                //Build a nested property expression of type T.School
+
+                Expression school = Expression.Property(pe, typeof(T).GetProperty("School"));
+
+                meLEA = Expression.Property(school, typeof(School).GetProperty("LA"));
+
+                meESTAB = Expression.Property(school, typeof(School).GetProperty("ESTAB"));
+            }
+
+            ConstantExpression constantLEA = Expression.Constant(LA, typeof(int));
+
+            ConstantExpression constantESTAB = Expression.Constant(ESTAB, typeof(int));
+
+            //Create the condition  
+            Expression body = Expression.AndAlso(
+                Expression.Equal(meLEA, constantLEA), 
+                Expression.Equal(meESTAB, constantESTAB));
+
+            //Create the expression data.where(s => s.School.LA == LA && s.school.ESTAB == ESTAB) 
+            //or if the type is School create the expression data.where(s => s.LA == LA && s.ESTAB == ESTAB) 
+            MethodCallExpression callExpression = Expression.Call(
+                    typeof(Queryable),
+                    "Where",
+                    new Type[] { data.ElementType },
+                    data.Expression,
+                    Expression.Lambda<Func<T, bool>>(body, new ParameterExpression[] { pe }));
+
+            // Return an executable query from the expression tree.
+            return data.Provider.CreateQuery<T>(callExpression);
+        }
+
+
     }
 }
