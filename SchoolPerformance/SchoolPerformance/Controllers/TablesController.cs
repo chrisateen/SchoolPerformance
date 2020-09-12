0e141a7a47316cb901d475582ac21f2290a69a87
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-12 20:36:44 +0100

    Redundant code comments removed from TableController

--- SchoolPerformance/SchoolPerformance/Controllers/TablesController.cs
+++ SchoolPerformance/SchoolPerformance/Controllers/TablesController.cs
@@ -42,57 +42,6 @@ namespace SchoolPerformance.Controllers
             return View(new TableViewModelDisadvantaged());
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> GetResultsAll()
-        //{
-        //    //Get results for all schools
-        //    var result = await _result.GetAll(r => r.OrderBy(s => s.School.SCHNAME), r => r.School);
-
-        //    //Get the national data
-        //    var nationalResultLst = await _result.GetNational();
-
-        //    //Because there is only currently national data for 2019 there should only be 1 result
-        //    var nationalResult = nationalResultLst.First();
-
-        //    //Converts from list of SchoolResult to List of TableViewModel
-        //    List<TableViewModelAll> resultViewModel = result.ConvertToTableViewModelAll();
-
-        //    //Convert national SchoolResult object to a TableViewModel object
-        //    TableViewModelAll resultNatViewModel = nationalResult;
-
-        //    var data = new { data = resultViewModel, national = resultNatViewModel };
-
-        //    return Json(data);
-        //}
-
-
-        //[HttpGet]
-        //public async Task<IActionResult> GetResultsDisadvantaged()
-        //{
-        //    //Get results for all schools 
-        //    //if the percentage of disadvantaged pupils is not null
-        //    var result = await _result.Get(
-        //        r => r.PTFSM6CLA1A != null,
-        //        r => r.OrderBy(s => s.School.SCHNAME),
-        //        r => r.School);
-
-        //    //Get the national data
-        //    var nationalResultLst = await _result.GetNational();
-
-        //    //Because there is only currently national data for 2019 there should only be 1 result
-        //    var nationalResult = nationalResultLst.First();
-
-        //    //Converts from list of SchoolResult to List of TableViewModel
-        //    List<TableViewModelDisadvantaged> resultViewModel = result.ConvertToTableViewModelDisadvantaged();
-
-        //    //Convert national SchoolResult object to a TableViewModel object
-        //    TableViewModelDisadvantaged resultNatViewModel = nationalResult;
-
-        //    var data = new { data = resultViewModel, national = resultNatViewModel };
-
-        //    return Json(data);
-        //}
-
         [HttpPost]
         public async Task<IActionResult> GetResultsAll()
         {
