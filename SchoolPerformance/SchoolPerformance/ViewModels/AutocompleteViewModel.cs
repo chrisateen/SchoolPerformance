c857a6cc5495b12dac0b3b825d30b0a78d05ffba
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-13 20:50:54 +0100

    AutocompleteViewModel class added

---
+++ SchoolPerformance/SchoolPerformance/ViewModels/AutocompleteViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.ViewModels
+{
+    public class AutocompleteViewModel
+    {
+        public int URN { get; set; }
+        public int LAESTAB { get; set; }
+        public string schoolName { get; set; }
+
+        public static implicit operator AutocompleteViewModel (SchoolResult result)
+        {
+            return new AutocompleteViewModel
+            {
+                URN = result.URN,
+                LAESTAB = result.School.LAESTAB,
+                schoolName = result.School.SCHNAME
+
+            };
+        }
+    }
+}
