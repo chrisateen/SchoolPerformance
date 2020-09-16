e45683e96d87489c6ccdb216e4d182de9df4c038
chrisateen <32651092+chrisateen@users.noreply.github.com>
2020-09-16 13:12:43 +0100

    ScatterplotListViewModel added

---
+++ SchoolPerformance/SchoolPerformance/ViewModels/ScatterplotListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPerformance.ViewModels
+{
+    public class ScatterplotListViewModel
+    {
+        public IEnumerable<ScatterplotViewModel> schoolData { get; set; }
+        public ScatterplotViewModel nationalData { get; set;}
+    }
+}
