eba3affd294c3d483e6e3058ae82caca454e8203
chrisateen <cawofe01@dcs.bbk.ac.uk>
2020-09-18 00:06:39 +0100

    Error controller test added

---
+++ SchoolPerformance/SchoolPerformaceTest/ControllerTest/ErrorControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentAssertions.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolPerformance.Controllers;
+using SchoolPerformance.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPerformanceTest.ControllerTest
+{
+    [TestClass]
+    public class ErrorControllerTest
+    {
+        //Checks 404 errors are handled
+        [TestMethod]
+        public void Error404ReturnsPageNotFound()
+        {
+            //Arrange
+            var controller = new ErrorController();
+
+            // Act and Assert
+            controller.Index(404).Should()
+                .BeViewResult().WithViewName("PageNotFound");
+        }
+
+        //Checks other errors are handled
+        [TestMethod]
+        public void ErrorReturnsErrorPage()
+        {
+            //Arrange
+            var controller = new ErrorController();
+
+            // Act and Assert
+            controller.Index(1).Should()
+                .BeViewResult().WithViewName("Error");
+        }
+
+    }
+}
